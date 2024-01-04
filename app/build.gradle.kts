24c14a12925e546109a288a5f5b67ca58678ef9a
Vera Sokolova <sokolik-0411@yandex.ru>
2024-01-04 22:16:08 +0500

    added ui test for density

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,9 +49,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // Room components
     implementation("androidx.room:room-ktx:2.6.1")
@@ -60,5 +57,11 @@ dependencies {
     //dagger
     implementation("com.google.dagger:dagger:2.49")
     kapt("com.google.dagger:dagger-compiler:2.49")
+
+    //testing
+    implementation("androidx.test.uiautomator:uiautomator:2.2.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
 }
---
+++ app/src/androidTest/java/ru/vsokolova/volumetric_table/DensityFragmentTest.kt
@@ -0,0 +1,52 @@
+package ru.vsokolova.volumetric_table
+
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.anything
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import ru.vsokolova.volumetric_table.ui.MainActivity
+
+@RunWith(AndroidJUnit4::class)
+class DensityFragmentTest {
+
+    @get:Rule val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    @get:Rule val disableAnimationRule = DisableAnimationsRule()
+
+    @Before
+    fun setUp() {
+        onView(withId(R.id.navigation_density)).perform(click())
+    }
+
+    @Test
+    fun checkSettingWoodType(){
+        onView(withId(R.id.spinner_wood_type)).perform(click())
+        onData(anything()).atPosition(2).inRoot(RootMatchers.isPlatformPopup()).perform(click())
+        onView(withId(R.id.spinner_wood_type)).check(matches(withText("Бук")))
+    }
+
+    @Test
+    fun checkSettingHumidity(){
+        onView(withId(R.id.spinner_humidity_value)).perform(click())
+        onData(anything()).atPosition(5).inRoot(RootMatchers.isPlatformPopup()).perform(click())
+        onView(withId(R.id.spinner_humidity_value)).check(matches(withText("40")))
+    }
+
+    @Test
+    fun checkSettingDensity(){
+        onView(withId(R.id.spinner_wood_type)).perform(click())
+        onData(anything()).atPosition(4).inRoot(RootMatchers.isPlatformPopup()).perform(click())
+        onView(withId(R.id.spinner_humidity_value)).perform(click())
+        onData(anything()).atPosition(3).inRoot(RootMatchers.isPlatformPopup()).perform(click())
+        onView(withId(R.id.text_view_result)).check(matches(withText("740 кг/м3")))
+    }
+}
